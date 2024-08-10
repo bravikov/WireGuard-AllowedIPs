dd4f1a29d28c821f6a2c0a521a7c17f76cf2a7b7
Dmitry Bravikov <bravikov@gmail.com>
2024-08-10 19:01:10 +0300

    Improve

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
+# WireGuard AllowedIPs Generator from domain names
+
+Generates a list of IP addresses from a list of domain names.
+
 # Dependency
 
-    git clone https://github.com/rthalley/dnspython
-    cd dnspython
-    python3 setup.py install
+    pip3 install -r requirements.txt
 
 # Usage
 
-1. You can create my-domains.list file.
+    python3 main.py
+
+1. You can create my.domain_list file.
 2. Add your domains to the file. One domain per line.
 3. Run main.py.
 
-Create other *.list files for other domains.
+Create other *.domain_list files for other domains.
 
 You may use '#' for comments in the *.list file.
 You may use empty line to separate domains group. 
--- main.py
+++ main.py
@@ -2,35 +2,49 @@ import dns.resolver
 import glob
 import sys
 import socket
+import re
 
-filenames = glob.glob('*.list')
 
-domains = []
+def read_lists(template):
+    filenames = glob.glob(template)
+    lines = []
+    for filename in filenames:
+        with open(filename) as file:
+            for line in file.readlines():
+                line = line.strip()
+                if not line:
+                    continue
+                if line.startswith('#'):
+                    continue
+                lines.append(line)
+    return lines
 
-for filename in filenames:
-    with open(filename) as file:
-        for line in file.readlines():
-            line = line.strip()
-            if not line:
-                continue
-            if line.startswith('#'):
-                continue
-            domains.append(line)
+
+domains = read_lists('*.domain_list')
 
 print('Domains:', domains)
 
 ipv4s = []
 ipv6s = []
 
+my_resolver = dns.resolver.Resolver()
+
+# 8.8.8.8 is Google's public DNS server
+my_resolver.nameservers = ['8.8.8.8']
+
 for domain in domains:
     try:
-        result = dns.resolver.resolve_name(domain)
+        result = my_resolver.resolve_name(domain)
     except dns.resolver.NXDOMAIN as e:
         print(e, file=sys.stderr)
+        exit(1)
     for ipv4 in result.addresses(family=socket.AF_INET):
         ipv4s.append(ipv4 + '/32')
     for ipv6 in result.addresses(family=socket.AF_INET6):
         ipv6s.append(ipv6 + '/128')
+
+ipv4s += read_lists('*.ipv4_list')
+ipv6s += read_lists('*.ipv6_list')
 
 print('IPv4:')
 print('\t', ', '.join(ipv4s))
